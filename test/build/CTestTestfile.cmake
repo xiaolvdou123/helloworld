25659654ebd983dd05ce8aa3d10c44db58baaa1e
xiaolvdou123 <773874013@qq.com>
2018-09-19 16:45:08 +0800

    delete test

--- test/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dell/C++Projects/test
-# Build directory: /home/dell/C++Projects/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
