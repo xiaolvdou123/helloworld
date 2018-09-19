1c728b42b27c8bc5435ac5a74656e388ba28c6a0
xiaolvdou123 <773874013@qq.com>
2018-09-19 16:27:46 +0800

    test

---
+++ test/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dell/C++Projects/test
+# Build directory: /home/dell/C++Projects/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
